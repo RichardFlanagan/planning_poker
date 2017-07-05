81ee229a329eccb1e988f88fec31eaea2cdfea56
Richard Flanagan <richard-flanagan@hotmail.com>
2017-07-05 23:12:25 +0100

    Refactor routes into a seperate module.n/master'.

--- app.js
+++ app.js
@@ -2,29 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const path = require('path');
+const routes = require('./routes');
+
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', routes);
+
 
 app.locals.site_name = 'planning_poker';
 
-
-app.use(function(req, res, next){
-	console.log('request');
-	next();
-});
-
-app.get('/', function(req, res){
-	res.render('homepage.pug');
-});
-
-app.get('/poker', function(req, res){
-	var context = {
-		'cards': [1,2,3]
-	};
-	res.render('poker.pug', context);
-});
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const routes = require('express').Router();
+
+// Basic middleware
+routes.use(function(req, res, next){
+	console.log('request');
+	next();
+});
+
+// Homepage
+routes.get('/', function(req, res){
+	res.render('homepage.pug');
+});
+
+// Poker page
+routes.get('/poker', function(req, res){
+	var context = {
+		'cards': [1,2,3]
+	};
+	res.render('poker.pug', context);
+});
+
+
+module.exports = routes;
